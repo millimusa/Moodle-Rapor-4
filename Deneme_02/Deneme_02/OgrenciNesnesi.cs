d20d972f9d4bebc613de74a9b19d257849183b4f
millimusa <32355532+millimusa@users.noreply.github.com>
2020-12-10 16:24:16 +0300

    Add files via upload

---
+++ Deneme_02/Deneme_02/OgrenciNesnesi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme_02
+{
+    class OgrenciNesnesi
+    {
+        private String ogrenciNo;
+        private String ogrenciAd;
+        private DateTime sonGiris;
+
+
+        public OgrenciNesnesi()
+        {
+            this.sonGiris = DateTime.MinValue;
+        }
+
+
+        public String getOgrenciNo()
+        {
+            return this.ogrenciNo;
+        }
+        public void setOgrenciNo(String ono)
+        {
+            this.ogrenciNo = ono;
+        }
+
+
+
+        public String getOgrenciAd()
+        {
+            return this.ogrenciAd;
+        }
+        public void setOgrenciAd(String oad)
+        {
+            this.ogrenciAd = oad;
+        }
+
+
+        public DateTime getSonGiris()
+        {
+            return this.sonGiris;
+        }
+
+        public void setSonGiris(DateTime sg)
+        {
+            this.sonGiris = sg;
+        }
+
+    }
+}
